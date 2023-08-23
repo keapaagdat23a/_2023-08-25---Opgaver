035e9bf1cafb4988c7d2ef175de9a4071780120d
PAAG <keapaagdat23a@gmail.com>
2023-08-23 22:23:43 +0200

    New assignment file

---
+++ src/TodaysAssignments.java
@@ -0,0 +1,54 @@
+public class TodaysAssignments {
+  public static void main(String[] args) {
+
+    // UDSKRIVE PÅ SKÆRMEN
+    System.out.println("\n\n\nUDSKRIVE PÅ SKÆRMEN");
+
+    int age = 23;
+    // Udskriv teksten "I am 23 years old." ved brug af variablen age
+
+
+
+    // DATATYPER
+    System.out.println("\n\n\nDATATYPER");
+
+    // Opret variabler med passende datatyper til disse informationer:
+    // Navn
+    // Fødselsår
+    // Alder (beregnet ud fra fødselsår)
+
+
+    // STRING
+    System.out.println("\n\n\nSTRING");
+    // Opret tekststrengen name og lad den indeholde dit fornavn eller kælenavn (mindst 4 karakterer)
+    // Udskriv længden af strengen
+    // Udskriv den første karakter i strengen
+    // Udskriver den sidste karakter i strengen
+    // Udskriv en version af strengen, hvor alle bogstaver er ændret til store bogstaver.
+    // Udskriv karakter nr. 2 i strengen
+    // Hvis dit navn er PATTY, så udskriv AT - altså 2. og 3. karakter i strengen, men uden at bruge charAt()
+
+
+
+    // TYPECASTING
+    System.out.println("\n\n\nTYPECASTING");
+    // Opret weight som et kommatal og initialisér den med værdien 67,8
+    // Udskriv tallet som et heltal, altså 67
+    // Kan du også udskrive tallet med to decimaler, altså 67,80
+
+
+
+    // DE 4 REGNEARTER +, -, * OG / SAMT MODULO (%)
+    System.out.println("\n\n\nDE FIRE REGNEARTER SAMT MODULO");
+    // Opret firstNumber som 4.5 og secondNumber som 2.2
+    // Udskriv resultatet af at bruge +, +, *, / og % på tallene
+    // Hvad er heltalsresten af  100 delt med 24?
+
+
+
+    // MATH-KLASSEN
+    System.out.println("\n\n\nMATH-KLASSEN");
+    // Find de fem metoder, som du forestiller dig at du kommer til at bruge oftest og lav et eksempel med hver
+
+  }
+}
